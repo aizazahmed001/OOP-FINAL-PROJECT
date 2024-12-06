3b38ca89c46ecb9642ae57923c4f37be1c0e75aa
AIZAZ KHAN <01-131232-106@student.bahria.edu.pk>
2024-12-06 12:56:45 +0500

    class remaining codeAdd project files.

---
+++ CAT ROAD CROSSER.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+class Cat {
+private:
+    Sprite sprite;
+    Texture texture;
+    float speed;
+
+public:
+    Cat(const string& textureFile, float speed, float scale) : speed(speed) {
+        if (!texture.loadFromFile(textureFile)) {
+            cerr << "Error loading Cat texture" << endl;
+        }
+        sprite.setTexture(texture);
+        sprite.setScale(scale, scale);
+        sprite.setPosition(700, 550);
+    }
